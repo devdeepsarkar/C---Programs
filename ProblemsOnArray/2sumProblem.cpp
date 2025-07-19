797754849e6f5c66a41abdf4606ce8ca713e585e
Devdeep <devdeepsarkar1234@gmail.com>
2025-07-19 13:00:05 +0530

    yoo

--- ProblemsOnArray/2sumProblem.cpp
+++ ProblemsOnArray/2sumProblem.cpp
@@ -13,7 +13,8 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    // brute force: T.C. = O(n^n), S(n) = O(1)
+    vector<int> brutetwoSum(vector<int>& nums, int target) {
         vector<int> ans;
         for(int i=0; i<nums.size(); i++) {
             for(int j=i+1; j<nums.size(); j++) {
@@ -24,9 +25,45 @@ public:
                 }
             }
         }
+        return {-1,-1};
+    }
+
+
+    // better soln: T.C. = O(n^logn)-->Ordered Map, S(n) = O(n)
+    vector<int> bettertwoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int> ans;
+        map<int, int> mpp;
+        for(int i=0; i<n; i++) {
+            int num1 = nums[i];
+            int need = target - num1;
+            if(mpp.find(need) != mpp.end()) {
+                ans.push_back(i);
+                ans.push_back(mpp[need]);
+                break;
+            }
+            else mpp[nums[i]] = i; 
+            
+        }
         return ans;
     }
 }s1;
+
+// optimal soln: only if we need to return the numbers or need to return only yes/no and not the indexes
+// T.C = O(N + NlogN), S(n) = O(1)
+string optimaltwosum(vector<int>& nums, int target) {
+    int n = nums.size();
+    int left = 0;
+    int right = n - 1;
+    sort(nums.begin(), nums.end());
+    while(left<right) {
+        int sum = nums[left] + nums[right];
+        if(sum == target) return "YES";
+        else if(sum > target)    right--;
+        else    left++;   
+    }
+    return "NO";
+}
 
 int main(){
 
@@ -41,11 +78,28 @@ int main(){
     int target;
     cin >> target;
 
-    vector<int> ans;
-    ans = s1.twoSum(arr, target);
+    vector<int> ans1;
+    ans1 = s1.brutetwoSum(arr, target);
+    cout << ans1[0] << " " << ans1[1] << endl;
 
-    cout << ans[0] << " " << ans[1];
+    vector<int> ans2;
+    ans2 = s1.brutetwoSum(arr, target);
+    cout << ans2[0] << " " << ans2[1] << endl;
+
+    cout << optimaltwosum(arr, target);
+    
     
 
     return 0;
 }
+
+/*
+i/p: 5
+     2 7 11 15 3
+     10
+
+o/p: 1 4
+     1 4
+     YES
+
+*/
